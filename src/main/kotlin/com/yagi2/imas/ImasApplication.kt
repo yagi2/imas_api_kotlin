ae80b501838bea4303e6fbc6a93031aa4f0867c8
Itsuki Aoyagi <mail@yagi2.com>
2017-08-31 12:01:52 +0900

    initial commit from SPRING INITIALIZER

---
+++ src/main/kotlin/com/yagi2/imas/ImasApplication.kt
@@ -0,0 +1,11 @@
+package com.yagi2.imas
+
+import org.springframework.boot.SpringApplication
+import org.springframework.boot.autoconfigure.SpringBootApplication
+
+@SpringBootApplication
+class ImasApplication
+
+fun main(args: Array<String>) {
+    SpringApplication.run(ImasApplication::class.java, *args)
+}
---
+++ src/test/kotlin/com/yagi2/imas/ImasApplicationTests.kt
@@ -0,0 +1,16 @@
+package com.yagi2.imas
+
+import org.junit.Test
+import org.junit.runner.RunWith
+import org.springframework.boot.test.context.SpringBootTest
+import org.springframework.test.context.junit4.SpringRunner
+
+@RunWith(SpringRunner::class)
+@SpringBootTest
+class ImasApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
